caa4c4015ce3a86fc50f75e0b7119f4904d148d1
Yu Hsuan <t0336470@gmail.com>
2021-04-14 14:23:58 +0200

    v1.15 update library

--- turtleHelper.py
+++ turtleHelper.py
@@ -15,7 +15,7 @@ LoginSuccess = 1
 LoginFailed = 2
 PushComplete = 3
 
-VERSION = 'v1.14'
+VERSION = 'v1.15'
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
