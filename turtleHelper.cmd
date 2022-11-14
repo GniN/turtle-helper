dcf6625f8861693bedc66650a9a2b1631be02a05
Yu Hsuan <t0336470@gmail.com>
2022-11-14 21:08:51 +0100

    test

---
+++ turtleHelper.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'turtleHelper.exe' with Python DLL being found.
+set PATH=c:\users\t0336\appdata\local\programs\python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\t0336\AppData\Local\Programs\Python\Python310
+"%~dp0.\turtleHelper.exe" %*
